604b36e6e406f50a536cc660e738127c5510658b
Nicola G <frukost@gmail.com>
2014-03-19 23:08:03 +0100

    Ethernet test base commit

---
+++ ethernet_test/ethernet_test.ino
@@ -0,0 +1,17 @@
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+#include <util.h>
+
+#include <aJSON.h>
+
+void setup() {
+  /* setup the application */
+}
+
+void loop() {
+  /* main application loop */
+}
